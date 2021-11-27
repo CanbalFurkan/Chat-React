f788a0bede3c8bfc02c67aa022435f30b34a15d9
furkan.canbal <furkan.canbal@airties.com>
2021-11-27 15:47:17 +0300

    Send Message function is added+resized message box

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 
 import {useCollectionData} from 'react-firebase-hooks/firestore';
+import { useState } from 'react';
 
 
 
@@ -22,32 +23,16 @@ firebase.initializeApp({
 const firestore=firebase.firestore();
 
 function App() {
-  return (
-    
-    <div className="container">
 
+
+
+  return (
+    <div>
       <section>
         <Chatroom/>
       </section>
-     
-      
-
-
-      <div class="input-group">
-  <input type="text" class="form-control"/>
-  <button class="btn btn-outline-secondary" type="button">
-  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
-  <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z"/>
-</svg>
-</button>
-</div>
-  
-  
-   
-              
- 
-     
-    </div>
+      </div>
+        
     
     
 
@@ -62,11 +47,33 @@ function Chatroom(){
 
   const [messages] = useCollectionData(query, { idField: 'id' });
 
+  const [message_text,set_message]=useState("");
+  const sendMessage= async(e) => {
+    await messagesRef.add({
+      text:message_text,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+
+    })
+
+   
+
+  }
+
 return (
 <>
+<div className="container">
 <div>
   {messages &&messages.map(msg=> <ChatMessage key={msg.id} message={msg}/>)}
 
+</div>
+<div class="input-group">
+  <input value={message_text} onChange={(e)=>set_message(e.target.value)} class="form-control"/>
+  <button class="btn btn-outline-secondary" type="button" onClick={sendMessage}>
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
+  <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z"/>
+  </svg>
+  </button>
+  </div>
 </div>
 </>
 
@@ -76,9 +83,6 @@ return (
 
 function ChatMessage(props){
 const{text,createdAt,uid}=props.message;
-console.log(createdAt.seconds);
-var current_date=new Date(createdAt.seconds*1000);
-console.log(current_date);
 return (
 <div class="imessage">
 <p class="from-me">{text}</p>
